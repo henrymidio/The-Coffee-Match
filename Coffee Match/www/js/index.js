230ed356407f310f08e6e655d7ec2352ec1aeb65
Henrique Emidio <henriquehemidio@gmail.com>
2017-01-18 21:47:01 -0200

    minusculo png

--- Coffee Match/www/js/index.js
+++ Coffee Match/www/js/index.js
@@ -47,7 +47,7 @@ var app = {
 		
 		var notificationReceivedCallback = function(jsonData) {
 			if(jsonData.notification.payload.additionalData.type == "invite") {
-				$$("#icon-invite").attr("src", "img/sino02.png")
+				$$("#icon-invite").attr("src", "img/sino02.PNG")
 			}	
  		};
 		
