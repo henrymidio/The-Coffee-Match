3ebaaf69ba893b699a0917c3d306d947d8ddbe78
Henrique Emidio <henriquehemidio@gmail.com>
2017-01-18 00:08:41 -0200

    fix dates

--- Coffee Match/www/js/my-app.js
+++ Coffee Match/www/js/my-app.js
@@ -998,7 +998,7 @@ function convertTo24(date){
 
 function getAge(dateString) {
     var today = new Date();
-    var birthDate = new Date(dateString);
+    var birthDate = new Date(dateString.replace(/-/g, "/"));
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
