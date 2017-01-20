af3155110ac89e9b3e8a951a7c545f632d19f82f
Henrique Emidio <henriquehemidio@gmail.com>
2017-01-20 02:25:37 -0200

    erro estranho fb

--- Coffee Match/www/js/jquery.jtinder.js
+++ Coffee Match/www/js/jquery.jtinder.js
@@ -51,9 +51,6 @@
 				var user_id    = localStorage.getItem("user_id");
 				var shown_user_id = panes.eq(current_pane).attr("id");
 				var message = localStorage.getItem("message");
-				if(!message){
-					message = "Hey! It seems we have similar interests. Let's have a coffee at Starbucks?!";
-				}
 								
 				localStorage.setItem("shown_user_id", shown_user_id);
 				var dados = {
