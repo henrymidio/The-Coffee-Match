85e4e96567ee12466fbb06e286e3bd33f6c2d9a3
Henrique Emidio <henriquehemidio@gmail.com>
2017-04-04 15:05:42 -0300

    testando o limit676788923143410

--- Coffee Match/www/js/jquery.jtinder.js
+++ Coffee Match/www/js/jquery.jtinder.js
@@ -317,6 +317,16 @@
 							});
 						} else {
 							panes.eq(current_pane).animate({"transform": "translate(-" + (pane_width) + "px," + (posY + pane_width) + "px) rotate(-60deg)"}, $that.settings.animationSpeed, function () {
+								var limit = localStorage.getItem("limit");
+								if(limit >= 0){
+									alert("Número de usuários limitados!")
+									lastPosX = 0;
+									lastPosY = 0;
+									panes.eq(current_pane).animate({"transform": "translate(0px,0px) rotate(0deg)"}, $that.settings.animationRevertSpeed);
+									panes.eq(current_pane).find($that.settings.likeSelector).animate({"opacity": 0}, $that.settings.animationRevertSpeed);
+									panes.eq(current_pane).find($that.settings.dislikeSelector).animate({"opacity": 0}, $that.settings.animationRevertSpeed);
+									return false;
+								}
 								if($that.settings.onDislike) {
 									$that.settings.onDislike(panes.eq(current_pane));
 								}
