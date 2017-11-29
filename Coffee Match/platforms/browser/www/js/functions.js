ed4edbaf93ed4789fe43427731750fcc18dc8319
Usuario <usuario@iMac-de-Usuario.local>
2017-11-29 20:06:06 -0200

    camera 8

--- Coffee Match/platforms/browser/www/js/functions.js
+++ Coffee Match/platforms/browser/www/js/functions.js
@@ -679,6 +679,21 @@ function updateStatusUser(status){
       // Show the selected image
       var smallImage = document.getElementById('picture');
       smallImage.src = imageURI;
+
+      var options = new FileUploadOptions();
+      options.fileKey="file";
+      options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+      options.mimeType="image/jpeg";
+
+      var params = new Object();
+      params.value1 = "test";
+      params.value2 = "param";
+
+      options.params = params;
+      options.chunkedMode = false;
+
+      var ft = new FileTransfer();
+      ft.upload(imageURI, "http://thecoffeematch.com/webservice/upload-photo.php", win, fail, options);
   }
 
   // A button will call this function
@@ -694,3 +709,15 @@ function updateStatusUser(status){
       destinationType: destinationType.FILE_URI,
       sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY });
   }
+
+
+function win(r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+    alert(r.response);
+}
+
+function fail(error) {
+    alert("An error has occurred: Code = ");
+}
